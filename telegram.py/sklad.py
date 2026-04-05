f6055cbcba362d542cf54fdfad676cf7ad8fa1dd
insult <katya546129.ru@mail.ru>
2026-04-05 14:54:48 +0300

    написала бота

---
+++ telegram.py/sklad.py
@@ -0,0 +1,20 @@
+﻿# 8799490361:AAFSrzHOAB3-oz5rQnyUTVjZpBkwJ9v7_CA
+# @insult_schedule_bot
+
+COMMAND_START_OUTPUT_TEXT = "Привет, мой дорогой друг! Я бот который поможет тебе узнать расписание."
+
+COMMAND_SCHEDULE_OUTPUT_TEXT = "Выбери день недели, на который хочешь узнать расписание"
+
+BUTTON_MONDAY_TEXT = "Понедельник"
+BUTTON_TUESDAY_TEXT = "Вторник"
+BUTTON_WEDNESDAY_TEXT = "Среда"
+BUTTON_THURSDAY_TEXT = "Четверг"
+BUTTON_FRIDAY_TEXT = "Пятница"
+BUTTON_SATURDAY_TEXT = "Суббота"
+
+SCHEDULE_MONDAY_OUTPUT_TEXT = "понедельник:\n1. классный час\n2. английский язык\n3. английский язык\n4. география\n5. алгебра\n6. черчение\n7. литература."
+SCHEDULE_TUESDAY_OUTPUT_TEXT = "вторник:\n1. физика\n2. история\n3. английский язык\n4. геометрия\n5. русский язык\n6. литература."
+SCHEDULE_WEDNESDAY_OUTPUT_TEXT = "среда:\n1. история\n2. физкультура\n3. английский язык\n4. обзр\n5. химия\n6. французский язык."
+SCHEDULE_THURSDAY_OUTPUT_TEXT = "четверг:\n1. биология\n2. русский язык\n3. алгебра\n4. информатика\n5. история\n6. геометрия."
+SCHEDULE_FRIDAY_OUTPUT_TEXT = "пятница:\n1. география\n2. русский язык\n3. алгебра\n4. литература\n5. физкультура\n6. английский язык."
+SCHEDULE_SATURDAY_OUTPUT_TEXT = "суббота:\n1. французский язык\n2. химия\n3. теория вероятностей и статистика\n4. обществознание\n5. биология\n6. физика."
---
+++ telegram.py/telegram_bot.py
@@ -0,0 +1,62 @@
+﻿import telebot
+import telebot.types
+import sklad
+
+
+bot = telebot.TeleBot("8799490361:AAFSrzHOAB3-oz5rQnyUTVjZpBkwJ9v7_CA")
+
+
+@bot.message_handler(commands=["start"])
+def start(message: telebot.types.Message):
+    bot.send_message(message.chat.id, sklad.COMMAND_START_OUTPUT_TEXT)
+
+
+@bot.message_handler(commands=["schedule"])
+def schedule(message: telebot.types.Message):
+    reply_keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    button_monday = telebot.types.KeyboardButton(sklad.BUTTON_MONDAY_TEXT)
+    button_tuesday = telebot.types.KeyboardButton(sklad.BUTTON_TUESDAY_TEXT)
+    button_wednesday = telebot.types.KeyboardButton(sklad.BUTTON_WEDNESDAY_TEXT)
+    button_thursday = telebot.types.KeyboardButton(sklad.BUTTON_THURSDAY_TEXT)
+    button_friday = telebot.types.KeyboardButton(sklad.BUTTON_FRIDAY_TEXT)
+    button_saturday = telebot.types.KeyboardButton(sklad.BUTTON_SATURDAY_TEXT)
+
+    reply_keyboard.add(button_monday)
+    reply_keyboard.add(button_tuesday)
+    reply_keyboard.add(button_wednesday)
+    reply_keyboard.add(button_thursday)
+    reply_keyboard.add(button_friday)
+    reply_keyboard.add(button_saturday)
+
+    bot.send_message(message.chat.id, sklad.COMMAND_SCHEDULE_OUTPUT_TEXT, reply_markup=reply_keyboard)
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message: telebot.types.Message):
+    input_text = message.text.lower()
+    output_text = ""
+
+    if input_text == sklad.BUTTON_MONDAY_TEXT.lower():
+        output_text = sklad.SCHEDULE_MONDAY_OUTPUT_TEXT
+        bot.send_message(message.chat.id, output_text)
+    elif input_text == sklad.BUTTON_TUESDAY_TEXT.lower():
+        output_text = sklad.SCHEDULE_TUESDAY_OUTPUT_TEXT
+        bot.send_message(message.chat.id, output_text)
+    elif input_text == sklad.BUTTON_WEDNESDAY_TEXT.lower():
+        output_text = sklad.SCHEDULE_WEDNESDAY_OUTPUT_TEXT
+        bot.send_message(message.chat.id, output_text)      
+    elif input_text == sklad.BUTTON_THURSDAY_TEXT.lower():
+        output_text = sklad.SCHEDULE_THURSDAY_OUTPUT_TEXT
+        bot.send_message(message.chat.id, output_text)
+    elif input_text == sklad.BUTTON_FRIDAY_TEXT.lower():
+        output_text = sklad.SCHEDULE_FRIDAY_OUTPUT_TEXT
+        bot.send_message(message.chat.id, output_text)
+    elif input_text == sklad.BUTTON_SATURDAY_TEXT.lower():
+        output_text = sklad.SCHEDULE_SATURDAY_OUTPUT_TEXT
+        bot.send_message(message.chat.id, output_text)
+    else:
+        bot.send_message(message.chat.id, "Пожалуйста, выберите день недели из предложенных кнопок.")
+
+print("Бот запущен...")
+bot.infinity_polling()
